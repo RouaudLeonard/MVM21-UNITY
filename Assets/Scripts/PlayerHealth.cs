4d516d34fcb331549164a52e39f8f26a91b65959
Naikym <mepower2001@gmail.com>
2023-09-08 20:53:51 -0500

    Started making PlayerHealth Script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float currentHealth;
+    float maxHealth = 100;
+    LayerMask Enemy;
+    float enemyDamage = 5f;
+   private Transform playerPos;
+    Transform enemyPos;
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyPos == playerPos)
+        {
+            currentHealth = currentHealth - enemyDamage;
+
+            Debug.Log("You are now at " + currentHealth + "HP");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+//if gets hit by enemy or enemy fire, deal damage.
+    }
+}
